031720523d8374d999659bde73c0d8ea1eb01d60
cloudchamb3r <jizon0123@protonmail.com>
2024-10-20 18:16:47 +0900

    fix: change table name for RepoEntity

--- src/main/kotlin/com/cloudchamb3r/minigit/repository/entity/RepoEntity.kt
+++ src/main/kotlin/com/cloudchamb3r/minigit/repository/entity/RepoEntity.kt
@@ -6,8 +6,10 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "repo")
 class RepoEntity(
     @field:Id
     @field:GeneratedValue

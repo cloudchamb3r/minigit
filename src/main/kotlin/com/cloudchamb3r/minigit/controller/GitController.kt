bf32a1e60534ac0421c37e550f38f21f4fdbcc0f
cloudchamb3r <jizon0123@protonmail.com>
2024-10-28 17:28:20 +0900

    feat: Add ReceivePack, UploadPack endpoint

--- src/main/kotlin/com/cloudchamb3r/minigit/controller/GitController.kt
+++ src/main/kotlin/com/cloudchamb3r/minigit/controller/GitController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,8 +24,8 @@ class GitController(
     fun getRefs(@PathVariable owner: String, @PathVariable repo: String, service: String?): ResponseEntity<String> {
         return gitService.transfer(owner, repo).let {
             when (service) {
-                "git-upload-pack" -> ResponseEntity(it.gitUploadPack(), HttpStatus.OK)
-                "git-receive-pack" -> ResponseEntity(it.gitReceivePack(), HttpStatus.OK)
+                "git-upload-pack" -> ResponseEntity(it.gitUploadPack(), HttpStatus.OK) // downloading data
+                "git-receive-pack" -> ResponseEntity(it.gitReceivePack(), HttpStatus.OK)// uploading data
                 else -> ResponseEntity(it.infoRefs(), HttpStatus.OK)
             }
         }
@@ -35,4 +36,13 @@ class GitController(
         return ResponseEntity(gitService.transfer(owner, repo).head(), HttpStatus.OK)
     }
 
+    @PostMapping("/{owner}/{repo}/git-receive-pack")
+    fun receivePack(@PathVariable owner: String, @PathVariable repo: String): ResponseEntity<Any> {
+        return ResponseEntity(null, HttpStatus.OK)
+    }
+
+    @PostMapping("/{owner}/{repo}/git-upload-pack")
+    fun uploadPack(@PathVariable owner: String, @PathVariable repo: String): ResponseEntity<Any> {
+        return ResponseEntity(null, HttpStatus.OK)
+    }
 }
